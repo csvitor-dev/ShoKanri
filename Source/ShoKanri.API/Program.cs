194dd279754da518723316727eb47d79fb2cc6bd
csvitor-dev <csvitor.dev@gmail.com>
2025-01-23 21:45:39 -0300

    build(`Source/`): add main projects to the application context

---
+++ Source/ShoKanri.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => 
+{
+    return Results.Ok(new { Message = "Hello World!" });
+})
+.WithName("Health Check")
+.WithOpenApi();
+
+app.Run();
