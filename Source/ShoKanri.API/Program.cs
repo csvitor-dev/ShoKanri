928c11ecbb66e05e16ca363d063a0b1c0882dc84
csvitor-dev <csvitor.dev@gmail.com>
2025-02-01 09:39:09 -0300

    chore: dump 'Minimal API' and use 'API Controller'

--- Source/ShoKanri.API/Program.cs
+++ Source/ShoKanri.API/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+[assembly: ApiController]
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => 
+{
+    options.LowercaseUrls = true;
+    options.LowercaseQueryStrings = true;
+});
 
 var app = builder.Build();
 
@@ -16,11 +26,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Helth check endpoint.
 app.MapGet("/", () => 
 {
     return Results.Ok(new { Message = "Hello World!" });
 })
 .WithName("Health Check")
 .WithOpenApi();
+
+app.MapControllers();
 
 app.Run();
