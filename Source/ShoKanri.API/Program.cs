736aacc6e6c92b6493145f1bf56310999237c2be
csvitor-dev <csvitor.dev@gmail.com>
2025-02-01 10:54:57 -0300

    feat: add built-in feature to monitor API health status

--- Source/ShoKanri.API/Program.cs
+++ Source/ShoKanri.API/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+using ShoKanri.API.Extensions;
 
 [assembly: ApiController]
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHealthChecks()
+    .AddResourceUtilizationHealthCheck();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,14 +28,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Use native Health Check service (extension)
+// of ASP.NET see https://learn.microsoft.com/en-us/dotnet/core/diagnostics/diagnostic-health-checks
+await app.Services.UseHealthCheckResourceAsync();
+
 app.UseHttpsRedirection();
 
-// Helth check endpoint.
+// Expose sample endpoint.
 app.MapGet("/", () => 
 {
-    return Results.Ok(new { Message = "Hello World!" });
+    return Results.Ok(new { 
+        Message = "Welcome to Shō-Kanri API, the sample API based on the Kanri project",
+        GitHubUrl = "https://github.com/csvitor-dev/ShoKanri.API",
+        ApiDocUrl = "https://localhost:7206/swagger"
+    });
 })
-.WithName("Health Check")
+.WithName("Sample Endpoint")
 .WithOpenApi();
 
 app.MapControllers();
