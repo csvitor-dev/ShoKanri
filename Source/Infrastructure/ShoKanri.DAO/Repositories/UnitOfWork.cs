963fed460c31faccc6aba615d0eb7f2c94fedda6
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-03-02 11:04:16 -0300

    UnitOfWork Initial implementation

---
+++ Source/Infrastructure/ShoKanri.DAO/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+using System;
+using ShoKanri.DAO.Context;
+using ShoKanri.Domain.Contracts.Data.Services;
+
+namespace ShoKanri.DAO.Repositories;
+
+//TODO: content provided to us insuficient, do futher research
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
