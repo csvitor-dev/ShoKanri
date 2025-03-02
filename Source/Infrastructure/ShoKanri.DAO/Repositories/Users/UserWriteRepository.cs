5b4bc9e3e31c31c750b3fb56f365f7dd6fd1523f
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-03-02 10:14:20 -0300

    UserWriteRepository

--- Source/Infrastructure/ShoKanri.DAO/Repositories/Users/UserWriteRepository.cs
+++ Source/Infrastructure/ShoKanri.DAO/Repositories/Users/UserWriteRepository.cs
@@ -1,8 +1,38 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using ShoKanri.DAO.Context;
+using ShoKanri.Domain.Contracts.Data.Repositories.User;
+using ShoKanri.Domain.Entities;
 
 namespace ShoKanri.DAO.Repositories.Users;
 
-public class UserWriteRepository
+public class UserWriteRepository : IUserWriteRepository
 {
+    private readonly AppDbContext _context;
 
+    public UserWriteRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task CreateAsync(User entity)
+    {
+        _context.Add(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        User user = await _context.Set<User>().FirstOrDefaultAsync(u => u.Id == id) ??
+                    throw new InvalidOperationException("That is no user with this id");
+
+        _context.Remove(user);
+        await _context.SaveChangesAsync();
+        
+    }
+
+    public async Task UpdateAsync(User entity)
+    {
+        _context.Update(entity);
+        await _context.SaveChangesAsync();
+    }
 }
