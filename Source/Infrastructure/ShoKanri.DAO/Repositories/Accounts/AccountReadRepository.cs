b1b2dd964ed9da9384bcc8ee1beb6e84bba30671
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-03-05 18:59:48 -0300

    Corrections on Account Repo

--- Source/Infrastructure/ShoKanri.DAO/Repositories/Accounts/AccountReadRepository.cs
+++ Source/Infrastructure/ShoKanri.DAO/Repositories/Accounts/AccountReadRepository.cs
@@ -1,31 +1,15 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using ShoKanri.DAO.Context;
+using ShoKanri.DAO.Repositories.Base;
 using ShoKanri.Domain.Contracts.Data.Repositories.Account;
 using ShoKanri.Domain.Entities;
 using ShoKanri.Domain.Entities.Transactions;
 
 namespace ShoKanri.DAO.Repositories.Accounts;
 
-public class AccountReadRepository : IAccountReadRepository
+public class AccountReadRepository(AppDbContext context) : ReadOnlyRepository<Account>(context), IAccountReadRepository
 {
-    private readonly AppDbContext _context;
-
-    public AccountReadRepository(AppDbContext context)
-    {
-        _context = context;
-    }
-
-    public async Task<List<Domain.Entities.Account>> FindAllAsync()
-    {
-        return await _context.Set<Account>().ToListAsync();
-    }
-
-    public async Task<Domain.Entities.Account?> FindByIdAsync(int id)
-    {
-        return await _context.Set<Account>().FirstOrDefaultAsync(u => u.Id == id);
-    }
-
     //Ask about this method
     public async Task<IList<Transaction>?> FindStatementForAccountAsync(int accountId)
     {
--- Source/Infrastructure/ShoKanri.DAO/Repositories/Accounts/AccountWriteRepository.cs
+++ Source/Infrastructure/ShoKanri.DAO/Repositories/Accounts/AccountWriteRepository.cs
@@ -1,48 +1,21 @@
-using System;
 using Microsoft.EntityFrameworkCore;
 using ShoKanri.DAO.Context;
+using ShoKanri.DAO.Repositories.Base;
 using ShoKanri.Domain.Contracts.Data.Repositories.Account;
 using ShoKanri.Domain.Entities;
 using ShoKanri.Domain.Entities.Transactions;
 
 namespace ShoKanri.DAO.Repositories.Accounts;
 
-public class AccountWriteRepository : IAccountWriteRepository
+public class AccountWriteRepository(AppDbContext _context) : WriteOnlyRepository<Account>(_context), IAccountWriteRepository
 {
-    private readonly AppDbContext _context;
-
-    public AccountWriteRepository(AppDbContext context)
-    {
-        _context = context;
-    }
-    public async Task CreateAsync(Account entity)
-    {
-        _context.Add(entity);
-        await _context.SaveChangesAsync();
-    }
-
     public async Task CreateTransactionForAccount(int accountId, Transaction transaction)
     {
         Account account = await _context.Set<Account>().FirstOrDefaultAsync(a => a.Id == accountId) ??
-                          throw new InvalidOperationException("Account not found");
+                          throw new InvalidOperationException("Id for entry inexistent, try another");
 
-        account.RegisterTransaction(transaction);
+        transaction.Execute(account);
+
         await UpdateAsync(account);
-    }
-
-    public async Task DeleteAsync(int id)
-    {
-        Account account = await _context.Set<Account>().FirstOrDefaultAsync(a => a.Id == id) ??
-                          throw new InvalidOperationException("Account not found");
-
-        _context.Remove(account);
-        await _context.SaveChangesAsync();
-
-    }
-
-    public async Task UpdateAsync(Account entity)
-    {
-        _context.Update(entity);
-        await _context.SaveChangesAsync();
     }
 }
