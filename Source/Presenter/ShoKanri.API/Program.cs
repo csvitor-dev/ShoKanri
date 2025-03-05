a56272acf7b8e3cfe337f71a65c0374e07c65087
csvitor-dev <csvitor.dev@gmail.com>
2025-03-05 17:34:09 -0300

    fix: update health check middleware to provide an endpoint in `/health`

--- Source/Presenter/ShoKanri.API/Program.cs
+++ Source/Presenter/ShoKanri.API/Program.cs
@@ -1,19 +1,19 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
-using ShoKanri.API.Extensions;
 
 [assembly: ApiController]
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddHealthChecks()
-    .AddResourceUtilizationHealthCheck();
+builder.Services.AddHealthChecks();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
-builder.Services.AddRouting(options => 
+builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
     options.LowercaseQueryStrings = true;
@@ -22,27 +22,28 @@ builder.Services.AddRouting(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-    // Expose sample endpoint.
-    app.MapGet("/", () => 
+// Expose sample endpoint.
+app.MapGet("/", () =>
     {
-        return Results.Ok(new { 
+        return Results.Ok(new
+        {
             Message = "Welcome to Shō-Kanri API, the sample API based on the Kanri project",
-            GitHubUrl = "https://github.com/csvitor-dev/ShoKanri.API",
-            ApiDocUrl = "https://localhost:7206/swagger"
+            GitHubUrl = "https://github.com/csvitor-dev/ShoKanri",
+            ApiDocUrl = "Go to the '/swagger' route to access the API documentation."
         });
     })
     .WithName("Sample Endpoint")
     .WithOpenApi();
-}
 
 // Use native Health Check service (extension)
 // of ASP.NET see https://learn.microsoft.com/en-us/dotnet/core/diagnostics/diagnostic-health-checks
-await app.Services.UseHealthCheckResourceAsync();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.UseHttpsRedirection();
 
