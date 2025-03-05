d5857a8e578da626dd30d1428f1e0c2eb76a8c5f
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-03-05 19:00:06 -0300

    Corrections on Users Repo

--- Source/Infrastructure/ShoKanri.DAO/Repositories/Users/UserReadRepository.cs
+++ Source/Infrastructure/ShoKanri.DAO/Repositories/Users/UserReadRepository.cs
@@ -1,31 +1,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using ShoKanri.DAO.Context;
+using ShoKanri.DAO.Repositories.Base;
 using ShoKanri.Domain.Contracts.Data.Repositories.User;
 using ShoKanri.Domain.Entities;
 
 namespace ShoKanri.DAO.Repositories.Users;
 
-public class UserReadRepository : IUserReadRepository
+public class UserReadRepository(AppDbContext _context) : ReadOnlyRepository<User>(_context), IUserReadRepository
 {
-    private readonly AppDbContext _context;
-
-    public UserReadRepository(AppDbContext context)
-    {
-        _context = context;
-    }
     public async Task<bool> FindActiveEmailAsync(string email)
     {
         return await _context.Set<User>().AnyAsync(u => u.Email == email);
-    }
-
-    public async Task<List<User>> FindAllAsync()
-    {
-        return await _context.Set<User>().ToListAsync();
-    }
-
-    public async Task<User?> FindByIdAsync(int id)
-    {
-        return await _context.Set<User>().FirstOrDefaultAsync(u => u.Id == id);
     }
 }
--- Source/Infrastructure/ShoKanri.DAO/Repositories/Users/UserWriteRepository.cs
+++ Source/Infrastructure/ShoKanri.DAO/Repositories/Users/UserWriteRepository.cs
@@ -1,38 +1,9 @@
-using System;
-using Microsoft.EntityFrameworkCore;
 using ShoKanri.DAO.Context;
+using ShoKanri.DAO.Repositories.Base;
 using ShoKanri.Domain.Contracts.Data.Repositories.User;
 using ShoKanri.Domain.Entities;
 
 namespace ShoKanri.DAO.Repositories.Users;
 
-public class UserWriteRepository : IUserWriteRepository
-{
-    private readonly AppDbContext _context;
-
-    public UserWriteRepository(AppDbContext context)
-    {
-        _context = context;
-    }
-    public async Task CreateAsync(User entity)
-    {
-        _context.Add(entity);
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task DeleteAsync(int id)
-    {
-        User user = await _context.Set<User>().FirstOrDefaultAsync(u => u.Id == id) ??
-                    throw new InvalidOperationException("That is no user with this id");
-
-        _context.Remove(user);
-        await _context.SaveChangesAsync();
-        
-    }
-
-    public async Task UpdateAsync(User entity)
-    {
-        _context.Update(entity);
-        await _context.SaveChangesAsync();
-    }
-}
+public class UserWriteRepository(AppDbContext _context) : WriteOnlyRepository<User>(_context), IUserWriteRepository
+{}
