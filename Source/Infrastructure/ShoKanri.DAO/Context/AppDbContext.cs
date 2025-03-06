6df42d740a24938941fb38972a29317559917afb
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-03-06 19:55:57 -0300

    removed EF configurations to use Fluent Migration instead

--- Source/Infrastructure/ShoKanri.DAO/Context/AppDbContext.cs
+++ Source/Infrastructure/ShoKanri.DAO/Context/AppDbContext.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Transactions;
 using Microsoft.EntityFrameworkCore;
-using ShoKanri.DAO.Context.EntitiesConfiguration;
 using ShoKanri.Domain.Entities;
 using ShoKanri.Domain.Entities.Transactions;
 
@@ -10,26 +7,10 @@ namespace ShoKanri.DAO.Context;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
-
     public DbSet<User> Users { get; set; }
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<Income> Incomes { get; set; }
-    public DbSet<Transference> Transferences { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfiguration(new UserConfigurations());
-        modelBuilder.ApplyConfiguration(new AccountConfigurations());
-        
-        modelBuilder.Entity<Domain.Entities.Transactions.Transaction>()
-            .HasDiscriminator<int>("MovimentKind")
-            .HasValue<Income>(0)
-            .HasValue<Expense>(1)
-            .HasValue<Transference>(2);
-
-        modelBuilder.Entity<Transference>().ToTable("Transferences");
-        
-        base.OnModelCreating(modelBuilder);
-    }
+    public DbSet<Transference> Transferences { get; set; } 
 }
+
