d791d40eb0461165ab27c32f64dbdc6c9cddf6c5
Rita <117872666+ritafranca@users.noreply.github.com>
2025-03-07 18:12:29 -0300

    Update CreateTransactionRequest.cs
    Com enum

--- Library/ShoKanri.Http/Requests/Transaction/CreateTransactionRequest.cs
+++ Library/ShoKanri.Http/Requests/Transaction/CreateTransactionRequest.cs
@@ -1,8 +1,10 @@
+using ShoKanri.Http.Enums;
+
 namespace ShoKanri.Http.Requests.Transaction;
 
 public class CreateTransactionRequest
 {
     public decimal Amount { get; set; }
     public string Description { get; set; }
-    public int TransactionType { get; set; } // 0=Income, 1=Expense, 2=Transfer
+    public TransactionType TransactionType { get; set; }
 }
