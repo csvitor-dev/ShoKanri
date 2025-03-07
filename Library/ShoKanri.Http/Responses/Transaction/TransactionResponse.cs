befd7543e092450718339186fce8be937dc992d4
Rita <117872666+ritafranca@users.noreply.github.com>
2025-03-07 18:09:42 -0300

    Update TransactionResponse.cs

--- Library/ShoKanri.Http/Responses/Transaction/TransactionResponse.cs
+++ Library/ShoKanri.Http/Responses/Transaction/TransactionResponse.cs
@@ -2,9 +2,10 @@ namespace ShoKanri.Http.Responses.Transaction;
 
 public class TransactionResponse
 {
-    public int TransactionId { get; set; }
-    public decimal Amount { get; set; }
-    public string Description { get; set; }
-    public int TransactionType { get; set; }
-    public DateTime CreatedOn { get; set; }
-}
+        public int Id { get; set; }
+	    public decimal Amount { get; set; }
+	    public string Description { get; set; }
+	    public TransactionType Type { get; set; }
+	    public DateTime CreatedOn { get; set; }
+    
+} 
