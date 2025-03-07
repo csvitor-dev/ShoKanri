c6494378824266640e352dfbca8957558b192b08
Rita <117872666+ritafranca@users.noreply.github.com>
2025-03-07 18:10:25 -0300

    Update Library/ShoKanri.Http/Requests/Transaction/CreateTransferRequest.cs
    Co-authored-by: Vitor Costa <118987544+csvitor-dev@users.noreply.github.com>

--- Library/ShoKanri.Http/Requests/Transaction/CreateTransferRequest.cs
+++ Library/ShoKanri.Http/Requests/Transaction/CreateTransferRequest.cs
@@ -4,6 +4,6 @@ public class CreateTransferRequest
 {
     public decimal Amount { get; set; }
     public string Description { get; set; }
-    public int SourceAccountId { get; set; }
-    public int DestinationAccountId { get; set; }
+    public int SourceId { get; set; }
+    public int DestinationId { get; set; }
 }
