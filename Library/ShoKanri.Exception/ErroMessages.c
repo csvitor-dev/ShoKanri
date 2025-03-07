d12488641e0e5a08ca5d9f4265cd121e4c6f6a84
Pedro Vinicius <pedrovinicius096@gmail.com>
2025-03-07 11:10:38 -0300

    Criação da pasta Base, e os arquivos InternalException e ProjectException

--- Library/ShoKanri.Exception/ErroMessages.c
+++
@@ -1,9 +0,0 @@
-namespace ShoKanri.Exception
-{
-    public static class MensagensDeErro
-    {
-        public const string ContaNaoEncontrada = "Conta não encontrada.";
-        public const string SaldoInsuficiente = "Saldo insuficiente para completar a operação.";
-        public const string OperacaoInvalida = "A operação solicitada não é válida.";
-    }
-}
