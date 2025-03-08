1126a73cd8aba412826dd895cf5b0d3550bf4dd3
Rita <117872666+ritafranca@users.noreply.github.com>
2025-03-07 23:06:49 -0300

    feat: Adicionar UserResponse para retorno de usuário

---
+++ Library/ShoKanri.Http/Responses/User/UserResponse.cs
@@ -0,0 +1,7 @@
+namespace ShoKanri.Http.Responses.User; 
+ 
+public class UserResponse { 
+    public int UserId { get; set; } 
+    public string Name { get; set; } 
+    public string Email { get; set; } 
+} 
