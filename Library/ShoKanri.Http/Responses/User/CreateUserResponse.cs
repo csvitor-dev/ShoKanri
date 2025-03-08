ddede70b1469bb48a15ee6f3f490cd8caa50854d
Rita <117872666+ritafranca@users.noreply.github.com>
2025-03-08 00:56:29 -0300

    feat: Adicionar CreateUserResponse, UpdateUserResponse e DeleteUserResponse; remover ErrorResponse e ValidationErrorResponse

---
+++ Library/ShoKanri.Http/Responses/User/CreateUserResponse.cs
@@ -0,0 +1,8 @@
+namespace ShoKanri.Http.Responses.User;
+
+public class CreateUserResponse
+{
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
---
+++ Library/ShoKanri.Http/Responses/User/UpdateUserResponse.cs
@@ -0,0 +1,7 @@
+namespace ShoKanri.Http.Responses.User;
+
+public class UpdateUserResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+}
