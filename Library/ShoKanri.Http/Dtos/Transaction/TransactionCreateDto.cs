4ad42186406f7d092299f90bc2f66806c7929d8d
csvitor-dev <csvitor.dev@gmail.com>
2025-03-08 17:31:22 -0300

    refactor: remove `.Dtos` layer

--- Library/ShoKanri.Http/Dtos/Transaction/TransactionCreateDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ShoKanri.Http.Dtos.Transaction;
-
-public class TransactionCreateDto
-{
-    public decimal Amount { get; set; }
-    public string Description { get; set; }
-    public TransactionType Type { get; set; } // 0=Income, 1=Expense, 2=Transfer
-}
--- Library/ShoKanri.Http/Dtos/User/UserCreateDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ShoKanri.Http.Dtos.User;
-
-public class UserCreateDto
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
