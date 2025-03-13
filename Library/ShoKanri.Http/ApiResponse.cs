2229c2a18b178c95a2b7b7ed3512ce54c4621753
csvitor-dev <csvitor.dev@gmail.com>
2025-03-13 11:14:45 -0300

    refactor: make `ApiResponse` more expressive and cleaner

--- Library/ShoKanri.Http/ApiResponse.cs
+++ Library/ShoKanri.Http/ApiResponse.cs
@@ -1,11 +1,10 @@
 namespace ShoKanri.Http;
 
-public class ApiResponse<T>
+public class ApiResponse
 {
-    public bool Success { get; set; }
-    public T? Data { get; set; }
-    public List<string> Errors { get; set; } = new();
+    public static dynamic Ok<T>(T data)
+        => new { Success = true, Data = data };
 
-    public static ApiResponse<T> Ok(T data) => new() { Success = true, Data = data };
-    public static ApiResponse<T> Fail(List<string> errors) => new() { Success = false, Errors = errors };
+    public static dynamic Fail(IList<string> errors)
+        => new { Success = false, Errors = errors };
 }
