ddac0e07a86b8ba0daa577d33a11cab6229bd8b2
csvitor-dev <csvitor.dev@gmail.com>
2025-03-17 12:12:02 -0300

    feat: implement routes to register and login an user

--- Source/Presenter/ShoKanri.API/Controllers/UserController.cs
+++ Source/Presenter/ShoKanri.API/Controllers/UserController.cs
@@ -1,13 +1,38 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ShoKanri.API.Services;
+using ShoKanri.Application.UseCases.User.Login;
+using ShoKanri.Application.UseCases.User.Register;
+using ShoKanri.Http.Requests.User;
+using ShoKanri.Http.Responses.User;
 
 namespace ShoKanri.API.Controllers;
 
 [Route("[controller]")]
-public sealed class UserController : ApplicationController
+public sealed class UserController
+    (TokenService service) : ApplicationController
 {
-    [HttpGet]
-    public IActionResult Get()
-        => AppOk(new { Message = typeof(UserController).FullName });
+    [HttpPost("register")]
+    [ProducesResponseType(typeof(RegisterUserResponse), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Register(
+        [FromServices] IRegisterUserUC uc,
+        [FromBody] RegisterUserRequest request)
+    {
+        var response = await uc.RegisterUser(request);
+
+        return AppCreated(response);
+    }
+
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(TokenResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Login(
+        [FromServices] ILoginUserUC uc,
+        [FromBody] LoginUserRequest request)
+    {
+        var result = await uc.LoginUser(request);
+        var token = service.GenerateJwtToken(result);
+
+        var response = new TokenResponse(result.Id, token);
+        return AppOk(response);
+    }
 }
