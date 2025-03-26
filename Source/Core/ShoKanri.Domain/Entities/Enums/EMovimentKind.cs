c3e802e4aee80279d2807181cdc5617606113798
csvitor-dev <csvitor.dev@gmail.com>
2025-03-26 13:19:37 -0300

    refactor: update transaction concept with kind of movement

---
+++ Source/Core/ShoKanri.Domain/Entities/Enums/EMovimentKind.cs
@@ -0,0 +1,8 @@
+namespace ShoKanri.Domain.Entities.Enums;
+
+public enum EMovementKind
+{
+    Expense = 1,
+    Income = 2,
+    Transference = 3
+}
--- Source/Core/ShoKanri.Domain/Entities/Transactions/Transaction.cs
+++ Source/Core/ShoKanri.Domain/Entities/Transactions/Transaction.cs
@@ -1,9 +1,13 @@
+using ShoKanri.Domain.Entities.Enums;
+
 namespace ShoKanri.Domain.Entities.Transactions;
 
 public abstract class Transaction : BaseEntity
 {
-    public int AccountId { get; init; }
+    public int AccountId { get; set; }
     public decimal Amount { get; set; }
+    public EMovementKind MovementKind { get; set; }
+    public string Description { get; set; } = string.Empty;
 
     public virtual Account? Account { get; set; }
 
